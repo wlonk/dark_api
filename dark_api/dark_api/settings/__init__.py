f1ae1d3061e2c6baaa8aabc15d1f30d8348ddd4a
Kit La Touche <kit.la.t@gmail.com>
2015-01-26 12:16:09 -0700

    Added CORS support.

--- dark_api/dark_api/settings/__init__.py
+++ dark_api/dark_api/settings/__init__.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'dark',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -86,3 +88,6 @@ STATIC_URL = '/static/'
 #     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
 #     'PAGINATE_BY': 10
 # }
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_URLS_REGEX = r'^/api/.*$'
