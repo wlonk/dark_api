30f6cdddf06763d658acf1f680a07f18cbf067ef
Kit La Touche <kit.la.t@gmail.com>
2015-04-19 23:31:55 -0600

    Initial tests and test harness.
    Just run `tox`! Or `py.test`.
    
    These tests need to be made better; this is just a sketch of what we'll
    be doing testwise.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dark API
+
+[![Circle CI](https://circleci.com/gh/wlonk/dark_api.svg?style=svg)](https://circleci.com/gh/wlonk/dark_api)
+
+This is the API for the Project Dark Rogues' Gallery.
---
+++ tests/test_api.py
@@ -0,0 +1,18 @@
+import pytest
+from django.contrib.auth import get_user_model
+from dark.models import (
+    Sheet,
+    Suit,
+)
+
+
+@pytest.fixture(scope='module')
+def user():
+    User = get_user_model()
+    return User.objects.create()
+
+
+@pytest.mark.django_db
+def test_create_sheet_for_user(user):
+    Sheet.objects.create_sheet_for_user(user)
+    assert Suit.objects.count() == 4
